e71bdad069b11103a3c96655a22e6371c591a63b
farukweb <faruk.webdev01@gmail.com>
2020-03-20 22:06:48 +0600

    Functional Componnent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Greet} from './components/Greet';
 
 function App() {
 
   var person ={
-    name: "Faruk Ahmad",
-    occupation: "Student"
+    
   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Person Name : {person.name} Person Occupation : {person.name}
-          </h3>
-        
-      </header>
+      <Greet/>
     </div>
-  );
-}
-
-export default App;
